5d70ef56de278b51e7a49fd970296c8eba69172b
g-soletta <giuliasoletta94@gmail.com>
2023-01-17 14:41:48 +0100

    giulia

--- _temporaneo_prova.py
+++
@@ -1,6 +0,0 @@
-import os
-listOfProjectIDs_abs_path = "home/casa/prova/oo/aaa"
-
-listOfProjectIDs_path = os.sep.join(os.path.normpath(listOfProjectIDs_abs_path).split(os.sep)[-2:])
-
-print(listOfProjectIDs_path)
