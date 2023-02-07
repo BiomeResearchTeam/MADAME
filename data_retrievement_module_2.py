e8b009b67d25ee284cfde6611a3d61ec0248e57f
Sara <fmglsara@gmail.com>
2023-02-07 11:27:43 +0100

    sara

--- data_retrievement_module_2.py
+++ data_retrievement_module_2.py
@@ -11,122 +11,126 @@ from rich.text import Text
 def data_retrievement(user_session):
 
     while True:
-        #Utilities.clear() 
+        ##Utilities.clear() 
         #title = " DATA RETRIEVEMENT MODULE "
         #print(Color.BOLD + Color.PURPLE + title.center(100, '-') + Color.END)
+        #Utilities.clear() 
         title = Panel(Text("DATA RETRIEVEMENT MODULE", style = "b magenta", justify="center"), style = "b magenta")
         rich_print(title)
         
         print("\nDownload the data associated to the previously downloaded metadata.\n\nChoose one of the following options:")
         print(" 1 - Use '*_merged_experiments-metadata.tsv' file present the current session")
         print(" 2 - Use '*_merged_experiments-metadata.tsv' files present in any other location of your computer")
+        print(" 3 - Input a file (tsv or csv format) with the list of runs you want to download")
         print("\n >>> Your current session is " + Color.BOLD + Color.YELLOW +f"{user_session}" + Color.END + " <<<\n")
         print(" --- If you want to return to the main menu digit: " + Color.BOLD + Color.PURPLE + "main menu" + Color.END + " ---\n")
-        user_data_input = input("\n>> Enter your choice: ").strip()
+        data_download_choice = input("\n>> Enter your choice: ").strip().lower()
 
-        if user_data_input in ("main menu", "MAIN MENU", "Main menu"):
+        if data_download_choice in ("main menu"):
             return
-
-        elif user_data_input.isnumeric() == False:
-            print(Color.BOLD + Color.RED + "\nWrong input" + Color.END, "expected a numeric input or <main menu> (without <>)\n\n")
-
-        elif user_data_input.isnumeric() == True:
-            user_data_input = int(user_data_input)
-            if user_data_input not in (1,2):
-                print("Error, enter a valid choice!\n")
-                return
+        elif data_download_choice.isnumeric():
+            data_download_choice = int(data_download_choice)
+            if data_download_choice not in (1,2,3):
+                print(Color.BOLD + Color.RED + "Error" + Color.END, "enter a valid choice!\n")
+                input("\nPress " + Color.BOLD + Color.PURPLE + f"ENTER" + Color.END + " to continue ")
 
             else:
-                if user_data_input == (1):
-                    data_user_session = os.path.join("Downloads", user_session)
-                    file_count = check_files(data_user_session)
-                    if file_count == 0:
-                        print(Color.BOLD + Color.RED + "\nError" + Color.END, "found 0 file. Are you sure the file is called '*_merged_experiments-metadata.tsv'? If not, please rename it\n")
+                if data_download_choice == 1:
+                    data_download_MADAME(user_session)
 
-                    elif file_count > 1:
-                        print(Color.BOLD + Color.RED + "\nError" + Color.END, "found too many files. Please choose a folder containing only 1 '*_merged_experiments-metadata.tsv'")
+                if data_download_choice == 2:
+                    data_download_path(user_session)
+        
+        else:
+            print(Color.BOLD + Color.RED + "Error" + Color.END, "enter a valid choice!\n")
+            input("\nPress " + Color.BOLD + Color.PURPLE + f"ENTER" + Color.END + " to continue ")
 
-                    else:
-                        print("\nWhat data format do you want to download? fastq, sra, or submitted")
+        
+        
+        # if user_data_input in ("main menu", "MAIN MENU", "Main menu"):
+        #     return
 
-                        user_file_type = ''
-                        while user_file_type.strip() == '': # preventing empty inputs (giulia)
-                            user_file_type = str(input(">> Enter your choice: ")).strip().lower()
-                            if user_file_type in ("main menu"):
-                                return
+        # elif user_data_input.isnumeric() == False:
+        #     print(Color.BOLD + Color.RED + "\nWrong input" + Color.END, "expected a numeric input or <main menu> (without <>)\n\n")
 
-                        while user_file_type not in ("fastq", "sra", "submitted", "main menu"):
-                            print(Color.BOLD + Color.RED +"\nWrong input " + Color.END, "Write <fastq>, <sra>, or <submitted> (without <>)\n")
+        # elif user_data_input.isnumeric() == True:
+        #     user_data_input = int(user_data_input)
+        #     if user_data_input not in (1,2):
+        #         print("Error, enter a valid choice!\n")
+        #         return
 
-                            user_file_type = ''
-                            while user_file_type.strip() == '': # preventing empty inputs (giulia)
-                                user_file_type = str(input(">> Enter your choice: ")).strip().lower()
-                                if user_file_type in ("main menu"):
-                                    return
+        #     else:
+#                 if data_download_choice == (1):
+#                     data_user_session = os.path.join("Downloads", user_session)
+#                     file_count = check_files(data_user_session)
+#                     if file_count == 0:
+#                         print(Color.BOLD + Color.RED + "\nError" + Color.END, "found 0 file. Are you sure the file is called '*_merged_experiments-metadata.tsv'? If not, please rename it\n")
 
-                        merged_experiments = check_file_experiments(data_user_session)
-                        e_df = read_experiments(data_user_session, merged_experiments)
-                        print()  #riga vuota prima dell'output di enaBT
-                        SequencesDownload.runDownloadData(user_session, e_df, file_type = user_file_type)
-                        return
+#                     elif file_count > 1:
+#                         print(Color.BOLD + Color.RED + "\nError" + Color.END, "found too many files. Please choose a folder containing only 1 '*_merged_experiments-metadata.tsv'")
+
+#                     else:
+#                         print("\nWhat data format do you want to download? fastq, sra, or submitted")
+
+#                         user_file_type = ''
+#                         while user_file_type.strip() == '': # preventing empty inputs (giulia)
+#                             user_file_type = str(input(">> Enter your choice: ")).strip().lower()
+#                             if user_file_type in ("main menu"):
+#                                 return
+
+#                         while user_file_type not in ("fastq", "sra", "submitted", "main menu"):
+#                             print(Color.BOLD + Color.RED +"\nWrong input " + Color.END, "Write <fastq>, <sra>, or <submitted> (without <>)\n")
+
+#                             user_file_type = ''
+#                             while user_file_type.strip() == '': # preventing empty inputs (giulia)
+#                                 user_file_type = str(input(">> Enter your choice: ")).strip().lower()
+#                                 if user_file_type in ("main menu"):
+#                                     return
+
+#                         merged_experiments = check_file_experiments(data_user_session)
+#                         e_df = read_experiments(data_user_session, merged_experiments)
+#                         print()  #riga vuota prima dell'output di enaBT
+#                         SequencesDownload.runDownloadData(user_session, e_df, file_type = user_file_type)
+#                         return
                                     
-                if user_data_input == (2):
-                    path = user_data_local(user_session)
-                    if path == 0:
-                        break
-                    file_count = check_files(user_session)
+#                 if data_download_choice == (2):
+#                     path = user_data_local(user_session)
+#                     if path == 0:
+#                         break
+#                     file_count = check_files(user_session)
                     
-                    if file_count == 0:
-                        print(Color.BOLD + Color.RED + "\nError" + Color.END, "found 0 file. Are you sure the file is called '*_merged_experiments-metadata.tsv'? If not, please rename it\n")
+#                     if file_count == 0:
+#                         print(Color.BOLD + Color.RED + "\nError" + Color.END, "found 0 file. Are you sure the file is called '*_merged_experiments-metadata.tsv'? If not, please rename it\n")
 
-                    elif file_count > 1:
-                        print(Color.BOLD + Color.RED + "\nError" + Color.END, "found too many files. Please choose a folder containing only 1 '*_merged_experiments-metadata.tsv'")
+#                     elif file_count > 1:
+#                         print(Color.BOLD + Color.RED + "\nError" + Color.END, "found too many files. Please choose a folder containing only 1 '*_merged_experiments-metadata.tsv'")
 
-                    else:
-                        print("\nWhat data format do you want to download? fastq, sra, or submitted")
+#                     else:
+#                         print("\nWhat data format do you want to download? fastq, sra, or submitted")
 
-                        user_file_type = ''
-                        while user_file_type.strip() == '': # preventing empty inputs (giulia)
-                            user_file_type = str(input(">> Enter your choice: ")).strip().lower()
-                            if user_file_type in ("main menu"):
-                                return
+#                         user_file_type = ''
+#                         while user_file_type.strip() == '': # preventing empty inputs (giulia)
+#                             user_file_type = str(input(">> Enter your choice: ")).strip().lower()
+#                             if user_file_type in ("main menu"):
+#                                 return
 
-                        while user_file_type not in ("fastq", "sra", "submitted", "main menu"):
-                            print(Color.BOLD + Color.RED +"\nWrong input " + Color.END, "Write <fastq>, <sra>, or <submitted> (without <>)\n")
+#                         while user_file_type not in ("fastq", "sra", "submitted", "main menu"):
+#                             print(Color.BOLD + Color.RED +"\nWrong input " + Color.END, "Write <fastq>, <sra>, or <submitted> (without <>)\n")
 
-                            user_file_type = ''
-                            while user_file_type.strip() == '': # preventing empty inputs (giulia)
-                                user_file_type = str(input(">> Enter your choice: ")).strip().lower()
-                                if user_file_type in ("main menu"):
-                                    return
+#                             user_file_type = ''
+#                             while user_file_type.strip() == '': # preventing empty inputs (giulia)
+#                                 user_file_type = str(input(">> Enter your choice: ")).strip().lower()
+#                                 if user_file_type in ("main menu"):
+#                                     return
 
-                        merged_experiments = check_file_experiments(data_user_session)
-                        e_df = read_experiments(data_user_session, merged_experiments)
-                        print()  #riga vuota prima dell'output di enaBT
-                        SequencesDownload.runDownloadData(user_session, e_df, file_type = user_file_type)
-                        return
+#                         merged_experiments = check_file_experiments(data_user_session)
+#                         e_df = read_experiments(data_user_session, merged_experiments)
+#                         print()  #riga vuota prima dell'output di enaBT
+#                         SequencesDownload.runDownloadData(user_session, e_df, file_type = user_file_type)
+#                         return
                         
 
-def user_data_local(user_session):
-    Utilities.clear()
-    while True:
-        print("Enter the path for '*_merged_experiments-metadata.tsv' file. \nData will be downloaded in the folder indicated.")
-        print("\n >>> Your current session is " + Color.BOLD + Color.YELLOW +f"{user_session}" + Color.END + " <<<\n")
-        print("\ --- If you want to return to the main menu digit: " + Color.BOLD + Color.PURPLE + "main menu" + Color.END + " ---\n") #verificare se è vero o se torna al report
-        user_data_local_path = input("\n>> Digit the path: ").strip()
 
-        if user_data_local_path in ("main menu", "MAIN MENU", "Main menu"):
-            return 0
-                            
-        if path.isdir(user_data_local_path) == False:
-            if path.isfile(user_data_local_path) == True:
-                print(Color.BOLD + Color.RED + "\nError. " + Color.END, "Please digit the path for the folder containing '*_merged_experiments-metadata.tsv' file\n\n")
-                return
-            else:
-                print(Color.BOLD + Color.RED + "\nFolder not found." + Color.END, " Maybe a typo? Try again\n\n")
-                return
-        else:
-            return user_data_local_path
 
 
 
@@ -148,3 +152,80 @@ def read_experiments(data_user_session, merged_experiments):
     path = os.path.join(data_user_session, merged_experiments)
     e_df = pd.read_csv(path, delimiter='\t', infer_datetime_format=True)
     return e_df
+
+
+#DATA IN MADAME
+def data_download_MADAME(user_session):
+    data_user_session = os.path.join("Downloads", user_session)
+    data_download_function(user_session, data_user_session)
+
+
+#DATA IN LOCAL PATH
+def data_download_path(user_session):
+    data_user_session = data_user_local(user_session)
+    data_download_function(user_session, data_user_session)
+
+def data_user_local(user_session):
+    Utilities.clear()
+    while True:
+        print("Enter the path for '*_merged_experiments-metadata.tsv' file. \nData will be downloaded in the folder indicated.")
+        print("\n >>> Your current session is " + Color.BOLD + Color.YELLOW +f"{user_session}" + Color.END + " <<<\n")
+        print("\ --- If you want to return to the main menu digit: " + Color.BOLD + Color.PURPLE + "main menu" + Color.END + " ---\n") #verificare se è vero o se torna al report
+        data_local_path = input("\n>> Digit the path: ").strip()
+
+        if data_local_path in ("main menu", "MAIN MENU", "Main menu"):
+            return
+                            
+        if path.isdir(data_local_path) == False:
+            if path.isfile(data_local_path) == True:
+                print(Color.BOLD + Color.RED + "\nError. " + Color.END, "Please digit the path for the folder containing '*_merged_experiments-metadata.tsv' file\n\n")
+                input("\nPress " + Color.BOLD + Color.PURPLE + f"ENTER" + Color.END + " to continue ")
+
+            else:
+                print(Color.BOLD + Color.RED + "\nFolder not found." + Color.END, " Maybe a typo? Try again\n\n")
+                input("\nPress " + Color.BOLD + Color.PURPLE + f"ENTER" + Color.END + " to continue ")
+                
+        else:
+            return data_local_path
+
+
+#DATA FROM USER FILE
+def data_download_CSV(user_session):
+    return
+
+
+#PRINCIPAL DOWNLOAD FUNCTION
+def data_download_function(user_session, data_user_session):
+    file_count = check_files(data_user_session)
+    
+    if file_count == 0:
+        print(Color.BOLD + Color.RED + "\nError" + Color.END, "found 0 file. Are you sure the file is called '*_merged_experiments-metadata.tsv'? If not, please rename it\n")
+
+    elif file_count > 1:
+        print(Color.BOLD + Color.RED + "\nError" + Color.END, "found too many files. Please choose a folder containing only 1 '*_merged_experiments-metadata.tsv'")
+
+    else:
+        print("\nWhat data format do you want to download? fastq, sra, or submitted")
+        print("\n >>> Your current session is " + Color.BOLD + Color.YELLOW +f"{user_session}" + Color.END + " <<<\n")
+        print(" --- If you want to return to the main menu digit: " + Color.BOLD + Color.PURPLE + "main menu" + Color.END + " ---\n")
+        data_download_type = input(">> Enter your choice: ").strip().lower()
+        if data_download_type in ("main menu"):
+            return
+        
+        elif data_download_type in ("fastq", "sra", "submitted"):
+            merged_experiments = check_file_experiments(data_user_session)
+            e_df = read_experiments(data_user_session, merged_experiments)
+            print()  #riga vuota prima dell'output di enaBT
+            SequencesDownload.runDownloadData(user_session, e_df, file_type = data_download_type)
+
+        else:
+            print(Color.BOLD + Color.RED +"\nWrong input " + Color.END, "Write <fastq>, <sra>, or <submitted> (without <>)\n")
+            input("\nPress " + Color.BOLD + Color.PURPLE + f"ENTER" + Color.END + " to continue ")
+
+
+#FINAL SCREEN                  
+def final_screen(user_session): #trovare dove metterla: penso dentro SequenceDownload
+    print("\nDOWNLOAD DATA completed!")
+    print("Now you can find the metadata files divided by projects inside the folder: MADAME/Downloads/" + Color.BOLD + Color.YELLOW + f"{user_session}" + Color.END)
+    input("\nPress " + Color.BOLD + Color.PURPLE + "ENTER" + Color.END + " to return to the main menu ")
+    return
